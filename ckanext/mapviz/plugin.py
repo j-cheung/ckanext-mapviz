e1a0e20645ad8500d82df9f895cf6872932c45a1
stupidjc <stupidjc@Johnsons-MacBook-Pro.local>
2018-01-20 19:36:49 +0000

    test retrieval

--- ckanext/mapviz/plugin.py
+++ ckanext/mapviz/plugin.py
@@ -3,11 +3,27 @@ import ckan.plugins.toolkit as toolkit
 
 
 class MapvizPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+	plugins.implements(plugins.IConfigurer)
+	plugins.implements(p.IResourceView, inherit=True)
+	plugins.implements(p.ITemplateHelpers, inherit=True)
 
-    # IConfigurer
+	# IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'mapviz')
+	def update_config(self, config_):
+		toolkit.add_template_directory(config_, 'templates')
+		toolkit.add_public_directory(config_, 'public')
+		toolkit.add_resource('fanstatic', 'mapviz')
+
+	# IResourceView
+	def info(self):
+	        return {'name': 'mapviz-view',# Name of plugin
+	                'title': 'Geovisualization',# Title to be displayed in interface
+	                'icon': 'globe',# Icon used.
+	                'iframed': False}
+
+	def can_view(self, data_dict): 
+		'''defines what types of files can use this view'''
+		return True
+
+	def view_template(self, context, data_dict):
+		return 'base.html'
