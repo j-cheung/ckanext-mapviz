a8e3edda71470fa4ab44dd876259daf553ac9626
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-01-21 15:55:28 +0000

    new module

--- ckanext/mapviz/fanstatic/js/mapviz_view.js
+++ ckanext/mapviz/fanstatic/js/mapviz_view.js
@@ -7,13 +7,8 @@
 ckan.module('mapvizview',function(jQuery,_) {
 
 	return{
-		options:{
-
-		},
 		initialize: function(){
       		console.log('Ive been called for element: %o', this.el);
 		}
-
-	}
-
+	};
 });
