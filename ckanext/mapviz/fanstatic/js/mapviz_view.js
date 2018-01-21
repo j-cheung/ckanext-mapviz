3fd5b341c7ba3ac1cc8ba772b93a43854ca232ca
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-01-21 17:19:01 +0000

    check template

--- ckanext/mapviz/fanstatic/js/mapviz_view.js
+++ ckanext/mapviz/fanstatic/js/mapviz_view.js
@@ -9,7 +9,6 @@ ckan.module('mapvizview',function(jQuery) {
 	return{
 		initialize: function(){
 			console.log("hello")
-      		console.log('Ive been called for element: %o', this.el);
 		}
 	};
 });
