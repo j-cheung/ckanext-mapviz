f94ae0c4f52e22f5dc182c548621519f87a1472a
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-01-31 00:10:33 +0000

    remove mapbox folder

--- ckanext/mapviz/fanstatic/js/vendor/mapbox-gl/test/render.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('flow-remove-types/register');
-
-const suite = require('./integration').render;
-const suiteImplementation = require('./suite_implementation');
-const ignores = require('./ignores.json');
-
-suite.run('js', ignores, suiteImplementation);
