0a33962f655b21cb73670101eb4bc5258e668ed2
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-02-06 21:57:13 +0100

    add mapbox

--- ckanext/mapviz/fanstatic/js/mapvizview.js
+++ ckanext/mapviz/fanstatic/js/mapvizview.js
@@ -10,51 +10,61 @@ ckan.module('mapvizview',function(jQuery) {
 		initialize: function(){
 			var self = this;
 
-      		this.el.append($("<div></div>").attr({
-      			"id":"map",
-      			"style":"width:100%; min-height:650px"
-      		}));
-      		// $("#map").append($("<div>HELLOOOOOOO</div>"));
+			this.el.append($("<div></div>").attr({
+				"id":"map",
+				"style":"width:100%; min-height:650px"
+			}));
+			// $("#map").append($("<div>HELLOOOOOOO</div>"));
 
-      		mapboxgl.accessToken = 'pk.eyJ1IjoiamNoZXVuZyIsImEiOiJjajh0M2FwMncwZ290MnFxdTY3enB6cXlnIn0.e26zB-gpmMOt6SjHbJ68vg';
+			mapboxgl.accessToken = 'pk.eyJ1IjoiamNoZXVuZyIsImEiOiJjajh0M2FwMncwZ290MnFxdTY3enB6cXlnIn0.e26zB-gpmMOt6SjHbJ68vg';
 			var map = new mapboxgl.Map({
-			    container: 'map',
-			    style: 'mapbox://styles/mapbox/streets-v9'
+				container: 'map',
+				style: 'mapbox://styles/mapbox/streets-v9'
 			});
 
-      		jQuery.getJSON(this.options.proxy_resource_url)
-      		.done(
-		        function(data){
-		        	console.log("success")
+			jQuery.getJSON(this.options.proxy_resource_url)
+			.done(
+				function(data){
+					console.log("success")
 					console.log(data);
 					//plot on map
 					self.plotGeoJSON(map,data)
-		        })
-		    .fail(
-		        function(jqXHR, textStatus, errorThrown) {
-		        	console.log("fail")
-	           	 	console.log(errorThrown)
-		        }
-		    );
+				})
+			.fail(
+				function(jqXHR, textStatus, errorThrown) {
+					console.log("fail")
+					console.log(errorThrown)
+				}
+			);
 		},
 
 		plotGeoJSON: function(map,jsonData) {
 			map.on('load', function() {
-				map.addSource({
+				map.addSource("resource-data",{
 					"type": "geojson",
 					"data": jsonData
 				});
 
+				map.addLayer({
+					"id": "park-boundary",
+					"type": "fill",
+					"source": "resource-data",
+					"paint": {
+						"fill-color": "#888888",
+						"fill-opacity": 0.4
+					},
+					"filter": ["==", "$type", "MultiLineString"]
+				});
 
 			})
 		},
 
 		showError: function (jqXHR, textStatus, errorThrown) {
-	      if (textStatus == 'error' && jqXHR.responseText.length) {
-	        this.el.html(jqXHR.responseText);
-	      } else {
-	        this.el.html(this.i18n('error', {text: textStatus, error: errorThrown}));
-	      }
-	    },
+		  if (textStatus == 'error' && jqXHR.responseText.length) {
+			this.el.html(jqXHR.responseText);
+		  } else {
+			this.el.html(this.i18n('error', {text: textStatus, error: errorThrown}));
+		  }
+		},
 	};
 });
