6a2eb5be9edac2e88a1e9831e681639c21ff641d
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-04-12 14:05:25 +0100

    test extension

---
+++ ckanext/mapviz/tests/test_mapviz.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+'''Tests for the ckanext.mapviz extension.
+
+'''
+
+from nose.tools import assert_raises
+from nose.tools import assert_equal
+
+import ckan.model as model
+import ckan.plugins
+from ckan.plugins.toolkit import NotAuthorized, ObjectNotFound
+import ckan.tests.factories as factories
+import ckan.logic as logic
+
+import ckan.tests.helpers as helpers
+
+import ckanext.mapviz.plugin as plugin
+
+class TestMapvizPlugin(object):
+    @classmethod
+    def setup_class(cls):
+        ckan.plugins.load('mapviz')
+        if not ckan.plugins.plugin_loaded('image_view'):
+            ckan.plugins.load('image_view')
+
+    def teardown(self):
+        model.repo.rebuild_db()
+
+    @classmethod
+    def teardown_class(cls):
+        ckan.plugins.unload('mapviz')
+        ckan.plugins.unload('image_view')
+
+    def test_can_view_with_format(self):
+        dataset = factories.Dataset()
+        resource = factories.Resource(  
+            package_id=dataset['id'],
+            url='http://dummy.link.data', 
+            format='geojson')
+        for resource_format in ['geojson', 'osm']:
+            data_dict = {'resource':{'url' : 'http://dummy.link.data',
+                                     'format' : resource_format}}
+            assert self.p.can_view(data_dict)
+        # resource_view = factories.ResourceView(
+        #     resource_id=resource['id'],
+        #     view_type='mapviz',
+
+
+        # assert_equal 
+
