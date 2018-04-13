a6ef0fb3e6e7794ac578a6f60a59420b9b9c602c
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-04-13 17:07:19 +0100

    test extension

--- ckanext/mapviz/tests/test_mapviz.py
+++ ckanext/mapviz/tests/test_mapviz.py
@@ -143,9 +143,9 @@ class TestMapvizPlugin(object):
 										 'hbase_table': 'table',
 										 'hbase_filename': 'filename'}}
 			mock_osm = "<osm></osm>"
-			import ckanext.mapviz.utils.readHBase as read
-			read.readOSM = mock.Mock(return_value=mock_osm)
-			mock.patch('utils.readHBase.readOSM',return_value=mock_osm)
+			# import ckanext.mapviz.utils.readHBase as read
+			# read.readOSM = mock.Mock(return_value=mock_osm)
+			mock.patch('ckanext.mapviz.utils.readHBase.readOSM',return_value=mock_osm)
 			expected_data = {'resource_url':resource_url,
 							 'resource_format':resource_format,
 							 'hbase_osm':mock_osm}
