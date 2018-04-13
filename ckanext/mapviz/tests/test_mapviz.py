cffebf6f3fa6211552f08690ab288ce3b9f0f544
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-04-13 18:48:21 +0100

    test extension

--- ckanext/mapviz/tests/test_mapviz.py
+++ ckanext/mapviz/tests/test_mapviz.py
@@ -126,8 +126,6 @@ class TestMapvizPlugin(object):
 			result_data = self.plugin.setup_template_variables(context=context,data_dict=data_dict)
 			assert_equal(result_data, expected_data)
 
-	import ckanext.mapviz.utils.readHBase as read
-	@mock.patch('read.readOSM',return_value="<osm></osm>")
 	def test_setup_teamplate_variables_proxy_hbase(self, patch):
 		self.plugin.proxy_enabled = True
 		mock_model = mock.MagicMock()
@@ -144,8 +142,8 @@ class TestMapvizPlugin(object):
 										 'hbase_table': 'table',
 										 'hbase_filename': 'filename'}}
 			mock_osm = "<osm></osm>"
-			# import ckanext.mapviz.utils.readHBase as read
-			# read.readOSM = mock.Mock(return_value=mock_osm)
+			import ckanext.mapviz.utils.readHBase as read
+			read.readOSM = mock.Mock(return_value=mock_osm)
 			# mock.patch('ckanext.mapviz.utils.readHBase.readOSM',return_value=mock_osm)
 			expected_data = {'resource_url':resource_url,
 							 'resource_format':resource_format,
